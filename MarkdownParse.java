dc08c6b5c44bfdc8cef8b8ecdcc58f6c121b4495
shc031 <103266876+shc031@users.noreply.github.com>
2022-04-15 08:56:55 -0700

    new test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,12 +12,15 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            System.out.println(currentIndex);
+           
         }
 
         return toReturn;
@@ -29,5 +32,11 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
+        /*
+        int i;
+        for(i = 0; i<content.length(); i++){
+            System.out.println(i + ": " + content.charAt(i));
+        }
+        */
     }
 }
---
+++ testfile.md
@@ -0,0 +1,4 @@
+# Test-2
+
+![Image](0fbb0991732bbe47e5b419c333b368e1_w48_h48.png)
+[CLICK](https://www.tutorialspoint.com/java/java_string_length.htm)
