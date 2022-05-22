689cf8e2e0dc62e20c9d0bc565d801ae4e3b4139
shc031 <103266876+shc031@users.noreply.github.com>
2022-05-21 23:33:17 -0700

    LabReportTask

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,9 +13,7 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             
-            
-            int exclamation = -1;
-            exclamation = markdown.indexOf("!");
+            int exclamation = markdown.indexOf("!");
             int openBracket = markdown.indexOf("[", currentIndex);
             if(openBracket == -1){
                 break;
@@ -24,6 +22,46 @@ public class MarkdownParse {
             if(closeBracket == -1){
                 break;
             }
+
+
+            //check if there is `` 
+            int Backtick = markdown.indexOf("`", currentIndex);
+            int nextline = markdown.indexOf("\n", Backtick);
+            if(Backtick != -1 && Backtick <= openBracket){
+                if(nextline != -1){
+                    int NextBacktick = markdown.indexOf("`", Backtick + 1);
+                    if(NextBacktick != -1 && NextBacktick <= nextline){
+                        currentIndex = NextBacktick + 1;
+                        continue;
+                    }
+                }
+            }
+
+            //check if it skip a line between ()
+            nextline = markdown.indexOf("\n", openBracket);
+            int howManyLine = 0;
+            while(nextline < closeBracket){
+                howManyLine += 1;
+                currentIndex = nextline + 1;
+                nextline = markdown.indexOf("\n", nextline + 1);
+            }
+            if(howManyLine > 2){
+                continue;
+            }
+            
+            
+            /*
+            System.out.println(Backtick + " and "+ openBracket);
+            if(Backtick != -1 && Backtick <= openBracket){
+                int NextBacktick = markdown.indexOf("`", Backtick + 1);
+                System.out.println(NextBacktick);
+                if(NextBacktick != -1){
+                    currentIndex = NextBacktick + 1;
+                    System.out.println("enter");
+                    continue;
+                }
+            }
+            */
             int openParen = markdown.indexOf("(", closeBracket);
             if(openParen == -1){
                 break;
@@ -32,28 +70,59 @@ public class MarkdownParse {
             if(closeParen == -1){
                 break;
             }
+            //checks if () is in ()
+            nextline = markdown.indexOf("\n", openParen);
+            int temp = markdown.indexOf("(", closeBracket);
+            while(temp < nextline && temp < closeParen && closeParen < nextline){
+                temp = markdown.indexOf("(", temp + 1);
+                if(temp > nextline || temp == -1  ||  temp > closeParen){
+                    break;}
+                closeParen = markdown.indexOf(")", closeParen + 1);
+            }
+
+            //check if it skip a line in []
+            nextline = markdown.indexOf("\n", openParen);
+            howManyLine = 0;
+            while(nextline < closeParen){
+                howManyLine += 1;
+                currentIndex = nextline + 1;
+                nextline = markdown.indexOf("\n", nextline + 1);
+            }
+            if(howManyLine > 2){
+                continue;
+            }
+
+
             if(exclamation == -1 ||  exclamation != openBracket - 1){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                nextline = markdown.indexOf("\n", openParen);
+                int end = markdown.indexOf("\n", nextline + 1);
+                //to contain URL without extra line
+                if(nextline < closeParen && nextline != -1 && end < closeParen && end != -1){
+                    toReturn.add(markdown.substring(nextline + 1, end - 1));
+                }
+                else{
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                }
             }
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
            
         }
 
         return toReturn;
     }
 
-
+    /*
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
-        /*
+        
         int i;
         for(i = 0; i<content.length(); i++){
             System.out.println(i + ": " + content.charAt(i));
         }
-        */
+        
     }
+    */
 }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParseTest{
+    /*
     @Test
     public void addition(){
         assertEquals(2,1+1);
@@ -21,4 +22,36 @@ public class MarkdownParseTest{
         toReturn.add("some-thing.html");
         assertEquals(toReturn, MarkdownParse.getLinks(content));
     }
+*/
+    @Test
+    public void labReportMDTest1() throws IOException{
+        Path fileName = Path.of("labReportTestFile1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("`google.com");
+        toReturn.add("google.com");
+        toReturn.add("ucsd.edu");
+        assertEquals(toReturn, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void labReportMDTest2() throws IOException{
+        Path fileName = Path.of("labReportTestFile2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("a.com");
+        toReturn.add("a.com(())");
+        toReturn.add("example.com");
+        assertEquals(toReturn, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void labReportMDTest3() throws IOException{
+        Path fileName = Path.of("labReportTestFile3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+        assertEquals(toReturn, MarkdownParse.getLinks(content));
+    }
+    
 }
